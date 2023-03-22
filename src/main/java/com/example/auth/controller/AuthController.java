82415023cba125b7c2679ebd0902ad73a1493267
hyuk89101 <git@khs871004>
2023-03-22 13:47:14 +0900

    CMIT

--- src/main/java/com/example/auth/controller/AuthController.java
+++ src/main/java/com/example/auth/controller/AuthController.java
@@ -2,13 +2,21 @@ package com.example.auth.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 public class AuthController {
-    @GetMapping(value = "/auth")
+    @GetMapping(value = "/auth/login")
     public String services() {
         return String.format("auth API Port : %s", "8013");
+    }
+
+    // message 메서드 추가
+    @GetMapping("/auth/message")
+    public String message(@RequestHeader("third-request") String header) {
+        log.info(header);
+        return "Hello World in auth.";
     }
 }
